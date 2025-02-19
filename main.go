3b08432c8d7fc6ff5fb9355b0d5b4851adea6979
berylatieno30 <berylatieno30@gmail.com>
2025-02-19 18:43:28 +0000

    feat: update entry

--- main.go
+++ main.go
@@ -2,19 +2,53 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"time"
 
-	"redis-monitor/internal/monitor"
+	"github.com/BerylCAtieno/redis-monitor/internal/monitor"
+	"github.com/BerylCAtieno/redis-monitor/internal/notifier"
 )
 
 func main() {
-	mon := monitor.NewRedisMonitor("localhost:6379")
+
 	ctx := context.Background()
 
-	memUsage, err := mon.GetMemoryUsage(ctx)
-	if err != nil {
-		log.Fatalf("Error fetching memory usage: %v", err)
+	// Optional: Add a timeout to prevent long-running operations
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel() // Ensure cleanup
+	redisMon := monitor.NewRedisMonitor("localhost:6379")
+
+	ticker := time.NewTicker(1 * time.Hour)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			memUsage, err := redisMon.GetMemoryUsage()
+			if err != nil {
+				log.Println("❌ Error fetching memory:", err)
+				continue
+			}
+
+			slowLogs, err := redisMon.GetSlowQueryCount(ctx)
+				if err != nil {
+    				log.Println("❌ Error fetching slow log count:", err)
+    				continue
+			}
+
+
+			cpuUsage, err := redisMon.GetCPUUsage()
+			if err != nil {
+				log.Println("❌ Error fetching CPU usage:", err)
+				continue
+			}
+
+			alertMessage := monitor.ShouldSendAlert(memUsage, slowLogs, cpuUsage)
+			if alertMessage != "" {
+				notifier.SendAlert(alertMessage)
+			} else {
+				log.Println("✅ No alert needed.")
+			}
+		}
 	}
-	fmt.Println("Memory Usage:\n", memUsage)
 }
